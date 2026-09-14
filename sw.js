4ab7f1682414f469646da7fa4dceebb9005df7bf
ontiverosadrian <tu-correo@gmail.com>
2026-09-14 08:03:55 -0600

    Actualizar rutas de iconos PWA a la carpeta images

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('glam-chic-v1').then((cache) => {
+    caches.open('glam-chic-v3').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
         '/app.js',
+        '/manifest.json',
+        '/images/icon-152.png',
+        '/images/icon-192.png',
         'https://cdn.tailwindcss.com',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
       ]);
